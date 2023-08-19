1b5263cb9e40ee754d8ffd12ff0562f13164cd7a
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-19 21:20:06 +0100

    created a simple calculator using react

---
+++ src/Components/calculatorValue.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const calculatorValue = [
+  "",
+  "C",
+  "⌫",
+  "/",
+  "×",
+  "7",
+  "8",
+  "9",
+  "-",
+  "4",
+  "5",
+  "6",
+  "+",
+  "1",
+  "2",
+  "3",
+  "=",
+  "0",
+  ".",
+ 
+];
+
+export default calculatorValue;
