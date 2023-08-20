3d34940b440b850023e4205f10e4294c322d4881
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-20 22:49:48 +0100

    edit

--- src/Components/divButtons.js
+++ src/Components/divButtons.js
@@ -16,7 +16,7 @@ const DivButtons = () => {
         setUpdate(result.toString());
       } catch (error) {}
     } else if (
-      (index >= 2 && index <= 18 && index !== 15) ||
+      (index >= 2 && index <= 18 && index !== 15 && update.length < 18) ||
       (index === 17 && !update.includes(".")) ||
       (index === 2 && !update.includes("/")) ||
       (index === 3 && !update.includes("*")) ||
